bf43dc5f101ecad5b17522d3e8b36e2023efa2dc
dcmm90 <dianacmontanes@gmail.com>
2017-08-18 18:03:39 +0100

    ttest

--- general_CV.py
+++ general_CV.py
@@ -67,7 +67,7 @@ def main():
     #[5000,10000,50000,100000,200000,300000,400000]
     features_num = [100,250,500,1000]
     for tissue in tissues:
-        feat_sel = 'fisher'
+        feat_sel = 't_test'
         open_file = os.path.realpath('../data_str/')
         ec = betaqn.loc[info[(info.tissue == tissue) & (info.braak_stage != 'Exclude')].index]
         cat = info['braak_bin'].loc[ec.index]
