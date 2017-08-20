6911a56e19bb6975f497d10ee45923d99b276a7a
dcmm90 <dianacmontanes@gmail.com>
2017-08-20 18:26:40 +0100

    stg

--- general_res_CV.py
+++ general_res_CV.py
@@ -53,11 +53,12 @@ def get_intervals(cv_splits, i, zeros, ones):
 
 
 def main():
-    tissue='CER'
+    tissue='STG'
+    
     open_file = os.path.realpath('../data_str/')
     ec, info = load_data(tissue)
     #,'fisher',
-    features_sel = ['fisher','rfe']
+    features_sel = ['t_test','fisher','rfe']
     #betaqn, info = load_data()
     #[100000, 50000, 1000, 500, 250, 100, 75, 50]
     #[5000,10000,50000,100000,200000,300000,400000]
@@ -65,8 +66,8 @@ def main():
     features_num = [20,50,75,100,250,500,1000,5000,10000,100000]
     #features_num = [50000,100000]
     for feat_sel in features_sel:
-        if feat_sel == 'fisher':
-            features_num = [50000]
+        if feat_sel == 't_test':
+            features_num = [100000]
         #feat_sel = 't_test'
         ec, info = load_data(tissue)
         #min_max_scaler = preprocessing.MinMaxScaler()
